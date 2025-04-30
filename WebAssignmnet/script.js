1e68b1b8df1a0950781441560221fc4b7d55d970
Imran Jaleel <imranjaleel2331@gmail.com>
2025-04-30 18:46:56 +0530

    used flex and many more topics

---
+++ WebAssignmnet/script.js
@@ -0,0 +1,7 @@
+function scrollToFooter() {
+    document.querySelector("footer").scrollIntoView({ behavior: "smooth" });
+  }
+ 
+  function  scrollToHome() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }

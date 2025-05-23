c0412d00bb47c975b277c837c574723ed1bb281a
Imran Jaleel <imranjaleel2331@gmail.com>
2025-05-23 18:13:25 +0530

    UserManganement Project

---
+++ UserManagement/script.js
@@ -0,0 +1,89 @@
+let itemsPerPage = 10;
+let skip = 0;
+let currentPage = 1;
+
+const userList = document.getElementById("userList");
+const searchInput = document.getElementById("searchInput");
+
+let currentData = [];
+let originalData = [];
+
+// Fetch and display users
+const fetchUsers = (items, skip) => {
+  fetch(`https://dummyjson.com/users?limit=${items}&skip=${skip}`)
+    .then((response) => response.json())
+    .then((data) => {
+      currentData = data.users;
+      originalData = data.users;
+      displayUsers();
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+};
+
+// Display users
+const displayUsers = () => {
+  userList.innerHTML = "";
+
+  currentData.forEach((user) => {
+    const div = document.createElement("div");
+    const btn = document.createElement("button");
+    div.className = "user";
+    btn.className = "deleteBtn";
+    div.innerHTML = `<strong>${user.firstName}</strong> <strong>${user.lastName}</strong> (${user.email}) `;
+    btn.innerText = "delete";
+    btn.addEventListener("click", deleteUser(user.id));
+    div.appendChild(btn);
+    userList.appendChild(div);
+  });
+};
+
+// Search users
+const searchUsers = () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (!searchTerm) {
+    currentData = originalData;
+    displayUsers();
+    return;
+  }
+
+  const filteredData = originalData.filter((user) =>
+    user.firstName.toLowerCase().includes(searchTerm)
+  );
+
+  if (filteredData.length > 0) {
+    currentData = filteredData;
+    displayUsers();
+  } else {
+    userList.innerHTML = "No users found.";
+  }
+};
+
+// Pagination controls
+const nextPage = () => {
+  skip += itemsPerPage;
+  fetchUsers(itemsPerPage, skip);
+};
+
+const previousPage = () => {
+  if (skip >= itemsPerPage) {
+    skip -= itemsPerPage;
+    fetchUsers(itemsPerPage, skip);
+  }
+};
+
+function deleteUser(id) {
+  const filteredData = originalData.filter((user) => user.id !== id);
+
+  if (filteredData.length > 0) {
+    currentData = filteredData;
+    displayUsers();
+  } else {
+    userList.innerHTML = "No users id found!!";
+  }
+}
+
+// Bind search input event
+searchInput.addEventListener("input", searchUsers);
+
+// Initial fetch
+fetchUsers(itemsPerPage, skip);
